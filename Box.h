56283c1817120673f37a0b1505a2de75b13e9796
IT21174476 <99313785+IT21174476@users.noreply.github.com>
2022-03-23 07:00:05 +0000

    Incomplete

--- Box.h
+++ Box.h
@@ -5,6 +5,9 @@ class Box {
        int height;
     public:
        // write prototypes of setters for length, width and height
+  void setLength (int l);
+  void setWidth (int w);
+  void setHeight (int h);
        // write prototypes of getters for length, width and height
  
        int calcVolume();
--- Student.h
+++ Student.h
@@ -1,9 +1,19 @@
-class Student {
-  // private section
-  //    int studentId
-  //    name <- 20 charcters
-  
+class Student 
+{
+    // private section
+    // int studentId
+    // name <- 20 charcters
+
+private:
+  int studentId;
+  char name[20];
+public:
+  void assignDetails(int sId, char sname[]);
+  void display();
+ 
   // public section
   //    assignDetails() method declaration
   //    display() method declaration
+
+
 };
